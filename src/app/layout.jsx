0ec970fb40dabb878598f90d86cbbfec6416adc1
FilipTakvam <filip.takvam@hotmail.com>
2024-02-27 14:29:41 +0100

    Added analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.scss';
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'], display: 'swap', adjustFontFallback: false})
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
           {children}
+          <Analytics />
       </body>
     </html>
   )
